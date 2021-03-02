436adfa2cfa1132bc481e965c74906984f10423e
unnati109c <67646140+unnati109c@users.noreply.github.com>
2021-03-02 12:47:31 +0530

    Binary Search on Answer

---
+++ Questions on Searching/binarysearch_square_and_squareroot_equation.cpp
@@ -0,0 +1,25 @@
+// https://codeforces.com/edu/course/2/lesson/6/2/practice/contest/283932/problem/E
+
+#include <bits/stdc++.h> 
+using namespace std;
+
+int main() {
+    long double l = 1, r = 1e18, c;
+    cin >> c;
+    long double mid;
+    for(int i=0; i<1000; i++) {
+        mid = (l+r)/2;
+        if(mid*mid + sqrt(mid) == c) {
+            break;
+        }
+        else if(mid*mid + sqrt(mid) > c) {
+            r = mid;
+        }
+        else {
+            l = mid;
+        }
+    }
+    cout << setprecision(16) << mid << "\n";
+
+    return 0;
+}
